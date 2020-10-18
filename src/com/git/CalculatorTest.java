56265abd192b97fa35aed7d7a65f5b5bd14100d5
anoop <anoopsing4991@gmail.com>
2020-10-18 20:27:53 +0530

    First Commit

---
+++ src/com/git/CalculatorTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WElcome to git");
+		
+	}
+
+}
